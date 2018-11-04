83a91ccf7b4a44a07258f07ad9d7015d86bc5954
Emilyf <emilygracefinch@outlook.com>
2018-11-03 21:16:01 -0400

    asdf

--- java/canvasjava.js
+++ java/canvasjava.js
@@ -1,4 +1,31 @@
 var canvas = document.getElementById("myCanvas"); 
-var ctx = canvas.getContext("2d");
-ctx.fillRect(0,0,150,75); 
+var md= false 
+canvas.addEventListener('mousedown',down);
+canvas.addEventListener('mouseup', toggledraw);
+canvas.addEventListener('mousemove',)
+function(evt){
+    var mousPos= getMousePos(canvs, evt);
+    var posx=mousePos.x;
+    var posy=mousePos.y;
+    draw(canvas, posx, posy);
+}
+function down (){
+    md=true
+}
+function toggledraw(){
+    md=false;
+}
+function getMousePos(canvas, evt){
+    var rect=canvas.getBoudingClientRect();
+    return{
+        x:evt.clientX - rect.left,
+        y:evt.clientY - rect.top
+    }
+}
+function draw(canvas,posx, posy){
+    var context= canvas.getContext('2d')
+    if (md){
+        context.fillRect(posx, posy, )
+    }
+}
 
