02c258a7eaac9f724b7d5b1ac0a0b677266b10d1
Emilyf <emilygracefinch@outlook.com>
2018-11-03 22:52:53 -0400

    please jesus

--- java/canvasjava.js
+++ java/canvasjava.js
@@ -1,12 +1,11 @@
-alert("hello");
 var canvas = document.getElementById('canvas'); 
 var context= canvas.getContext('2d');
 
 var radius=10; 
 
 var putPoint=function(e){
-    context.beginPath():
-    context.arc(e.clientX, e.clientY,radius,0,Math.PI*2);
+    context.beginPath();
+    context.arc(e.offsetX, e.offsetY,radius,0,Math.PI*2);
     context.fill(); 
 }
 canvas.addEventListener('mousedown', putPoint);
