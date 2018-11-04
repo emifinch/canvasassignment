341efbdd841c5bd6e617c4592a85b9e44e075909
Emilyf <emilygracefinch@outlook.com>
2018-11-03 23:03:06 -0400

    dragging

--- java/canvasjava.js
+++ java/canvasjava.js
@@ -2,11 +2,24 @@ var canvas = document.getElementById('canvas');
 var context= canvas.getContext('2d');
 
 var radius=10; 
+var dragging=false;
 
 var putPoint=function(e){
-    context.beginPath();
-    context.arc(e.clientX, e.clientY,radius,0,Math.PI*2);
-    context.fill(); 
+    if(dragging){
+        context.beginPath();
+        context.arc(e.clientX, e.clientY,radius,0,Math.PI*2);
+        context.fill();  
+    }
 }
-canvas.addEventListener('mousedown', putPoint);
 
+var engage=function(){
+    dragging=true;
+}
+
+var disengage=function(){
+    dragging=false;
+}
+
+canvas.addEventListener('mousedown',engage);
+canvas.addEventListener('mousedown', putPoint);
+canvas.addEventListener('mouseup',disengage);
