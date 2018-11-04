34f2c1b5b8d6f0d3476962c98d7ca2b07e787e5e
Emilyf <emilygracefinch@outlook.com>
2018-11-03 21:47:13 -0400

    as

--- java/canvasjava.js
+++ java/canvasjava.js
@@ -3,12 +3,12 @@ var context= canvas.getContext('2d');
 
 var radius=10; 
 
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight; 
+//canvas.width=window.innerWidth;
+//canvas.height=window.innerHeight; 
 
 var putPoint=function(e){
     context.beginPath():
-    context.arc(e.offsetX, e.offsetY,radius,0,Math.PI* 2);
+    context.arc(e.offsetX, e.offsetY,radius,0,Math.PI*2);
     context.fill(); 
 }
 canvas.addEventListener('mousedown', putPoint);
