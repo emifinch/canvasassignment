9f75dee92fbfe4c96ea21b36cba81fe44b01a547
Emilyf <emilygracefinch@outlook.com>
2018-11-03 21:32:38 -0400

    try this

--- java/canvasjava.js
+++ java/canvasjava.js
@@ -3,6 +3,9 @@ var context= canvas.getContext('2d')
 
 var radius=10; 
 
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight;
+
 var putPoint=function(e){
     context.beginPath():
     context.arc(e.offsetX, e.offsetY, radius,0,Math.PI=2);
