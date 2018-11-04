67c4bc13ba69342bcff77ae24f509673715cb11c
Emilyf <emilygracefinch@outlook.com>
2018-11-03 22:07:14 -0400

    push

--- java/canvasjava.js
+++ java/canvasjava.js
@@ -3,9 +3,6 @@ var context= canvas.getContext('2d');
 
 var radius=10; 
 
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight; 
-
 var putPoint=function(e){
     context.beginPath():
     context.arc(e.offsetX, e.offsetY,radius,0,Math.PI*2);
